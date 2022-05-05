e613ba61bbcb242c3d50577426784953c7c51229
Gyanendra <shahgyanendra123.gmail.com>
2022-05-05 10:53:19 +0530

    adding event handler

--- OneDrive/Desktop/react projects/Cart/src/CartItem.js
+++ OneDrive/Desktop/react projects/Cart/src/CartItem.js
@@ -1,18 +1,39 @@
 import React from "react";
 
 class CartItem extends React.Component{
+    constructor (){
+        super();
+        this.state ={
+            price:900,
+            title:'Phone',
+            qty:1,
+            img:''
+        }
+    }
+    increaseQuantity=()=>{ 
+        console.log('this',this.state);
+    }
     render(){
+        const {price,title,qty} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs 999</div>
-                    <div style={{fontSize:25}}>Qty:</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>Rs {price}</div>
+                    <div style={{fontSize:25}}>{qty}</div>
                     <div className="cart-item-actions">
                         {/*Buttons*/}
+                        <img alt="increase"
+                        className="action-icons" 
+                        src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+                        onClick={this.increaseQuantity}
+                        />
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png"/>
+                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"/>
+
                     </div>
                 </div>
 
